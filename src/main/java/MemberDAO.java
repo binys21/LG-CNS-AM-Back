ee98153982526672f690644617c960ca708ea32f
binys21 <binys1121@ewhain.net>
2025-01-21 23:09:05 +0900

    feat: 회원 등록 로직

---
+++ src/main/java/MemberDAO.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class MemberDAO {
+    private static long nextId=0;
+    //키: 이메일, 값: Member
+    private Map<String,Member>map =new HashMap<>();
+
+    //조회
+    public Member selectByEmail(String email){
+        return map.get(email);
+    }
+
+    //등록
+    public void insert(Member member){
+        member.setId(++nextId);
+        map.put(member.getEmail(),member);
+    }
+
+    //수정
+    public void update(Member member){
+        map.put(member.getEmail(),member);
+    }
+    //전체 조회
+    public Collection<Member> selectAll(){
+        return map.values();
+    }
+}
---
+++ src/main/java/MemberRegisterService.java
@@ -0,0 +1,19 @@
+import java.time.LocalDateTime;
+
+public class MemberRegisterService {
+    private MemberDAO memberDAO;
+
+    public MemberRegisterService(MemberDAO memberDAO) {
+        this.memberDAO=memberDAO;
+    }
+
+    public long regist(RegisterRequest req){
+        Member member = memberDAO.selectByEmail(req.getEmail());
+        if(member!=null){
+            throw new DuplicateMemberException("이메일 중복"+req.getEmail());
+        }
+        Member newMember=new Member(req.getEmail(), req.getPassword(), req.getName(), LocalDateTime.now());
+        memberDAO.insert(newMember);
+        return newMember.getId();
+    }
+}
